b4061e3e4955d2f19b16d94e63682e9826286dd9
Pawel Kadluczka <1438884+moozzyk@users.noreply.github.com>
2025-08-21 16:22:22 -0700

    Publish

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// If deploying to GitHub Pages at https://<user>.github.io/<repo>/,
-// set base: "/<repo>/"
 export default defineConfig({
   plugins: [react()],
-  // base: "/visual-minipro-site/"
+  base: "/visualminipro-web/"
 });
